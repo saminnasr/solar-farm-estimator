57cfe0cfc3dbe50d107f263a7949b32d7c3a972c
Samin <72705951+saminnasr@users.noreply.github.com>
2025-04-28 09:29:16 +0330

    Update APP_Pro.py

--- APP_Pro.py
+++ APP_Pro.py
@@ -278,7 +278,7 @@ with st.expander("➕ Enter Land Polygon Coordinates (Longitude, Latitude)"):
     for i in range(num_points):
         colx, coly = st.columns(2)
         with colx:
-            lon = st.number_input(f"Longitude {i+1}", key=f"lon_{i}", format="%.6f", step=0.000001)
+            lon = st.number_input(f"Longitude {i+1}", key=f"lon_{i}", format="%.6f", step=0.00001)
         with coly:
             lat = st.number_input(f"Latitude {i+1}", key=f"lat_{i}", format="%.6f", 0.00001)
         lon_coords.append(lon)
